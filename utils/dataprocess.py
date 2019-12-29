20968a617f4c96fc88c3cbb043ceab4471d0afb2
guojson <gxc@cau.edu.cn>
2019-12-29 10:27:36 +0800

    进行测试用的1070条数据集

--- utils/dataprocess.py
+++ utils/dataprocess.py
@@ -1,70 +1,59 @@
-#coding=utf8
-import io
-import sys
-
-import jieba
-
 
 from utils.SQLiteTools import ConnectSqlite
-import random
 
-# sys.stdout = io.TextIOWrapper(sys.stdout.buffer,encoding='utf8') #改变标准输出的默认编码
+con = ConnectSqlite()
+def gen_labels(id):
+    sql = 'SELECT * FROM category where id='+str(id)+';'
+    catogory =con.fetchall_table(sql)
+    return str(catogory[0][3])
+
+def _parse_anno(path,save_path):
+    try:
+        with open(save_path,'w',encoding='utf-8') as w:
+            with open(path,'r',encoding='utf-8') as f:
+                sentences=f.readlines()
+                data=[]
+                for count,sentence in enumerate(sentences):
+                    tags=[]
+                    category=[]
+                    first_tag=True
+                    for index,character in enumerate(list(sentence.strip())):
+                        # print(str(index)+'\t'+character+'\n')
+                        if len(character.strip())==0:
+                            continue
+                        if len(tags)!=0:
+                            if ''.join(tags[-2:])=='<e':
+                                if character!='>':
+                                    category.append(character)
+                                else:
+                                    tags.append(character)
+                            elif len(tags)==3:
+                                id=int(''.join(category))
+                                ann=gen_labels(id)
+                                if character !='<' and first_tag:
+                                    w.write(character+'\t'+'B-'+ann+'\n')
+                                elif character !='<' and not first_tag:
+                                    w.write(character + '\t' + 'I-' + ann+'\n')
+                                    first_tag=False
+                                else:
+                                    tags.append('<')
+                            elif len(tags)>=4 and ''.join(tags[-2:])=='/e':
+                                if character=='>':
+                                    tags.clear()
+                                    category.clear()
+                            else:
+                                tags.append(character)
+                        else:
+                            if character=='<' and sentence[index+1]=='e':
+                                tags.append(character)
+                            else:
+                                w.write(character + '\t' + 'O'+'\n')
+                    w.write('\n')
+                    index+=1
+    except Exception as e:
+        print("出现异常\n"+e)
+
 if __name__ == '__main__':
-    # filepath='../demotext/水稻玉米小麦大豆大麦.txt.pre'
-    # # with open(filepath+'.pre','a',encoding='utf-8') as f2:
-    # with open(filepath,'r',encoding='utf-8') as f:
-    #     for abstrct in f:
-    #         print(abstrct)
-    #         abstrct=abstrct.strip()
-    #         if abstrct.startswith('"'):
-    #             abstrct=abstrct[1:-1]
-    #             print(abstrct)
-    #             if abstrct.startswith('<正>'):
-    #                 abstrct=abstrct[3:].strip()
-    #                 abstrct=abstrct.replace(' ','')
-    #                 abstrct = abstrct.replace('""', '')
-    #                     # f2.write(abstrct+'\n')
-
-    # con=ConnectSqlite()
-    # path='../configs/total_dict.utf8'
-    #
-    # with open(path,'r',encoding='utf8') as f:
-    #       index=1
-    #       for line in f:
-    #         line=line.strip().replace('\n','')
-    #         data=line.split(' ')
-    #         data = str(index)+",'"+data[0]+"','"+data[1]+"'"
-    #         print('insert into lexicons(id,lexicon,categoryone) values (%s)' %data)
-    #         con.insert_update_table('insert into lexicons(id,lexicon,categoryone) values (%s)' %data)
-    #         index+=1
-    # sql="select count(*) from lexicons where lexicon='"
-    # with open('../configs/lexicon.txt','w',encoding='utf-8') as f2:
-    #     with open('../configs/sougou.txt','r',encoding='GBK') as f:
-    #         lines=f.readlines()
-    #         for line in lines:
-    #             word=line.strip().split(' ')[1]
-    #             data=con.fetchall_table(sql + word + "';")
-    #
-    #             if data[0][0]==0:
-    #                 print(word)
-    #                 f2.write(word + '\n')
-    #     datas = con.fetchall_table('''select lexicon from lexicons''')
-    #     for index in range(len(datas)):
-    #         print(datas[index][0])
-    #         f2.write(datas[index][0]+'\n')
-    #
-    #
-    #
-    #
-    #
-    # # seg_list = jieba.cut("我来到北京清华大学", cut_all=False)
-    # # print("精准模式: " + "/ ".join(seg_list))  # 精确模式
-
-    with open('../demotext/水稻玉米小麦大豆大麦.txt','r',encoding='UTF-8') as f:
-        lines=f.readlines()
-        print(len(lines))
-        random.shuffle(lines)
-        with open('../demotext/水稻玉米小麦大豆大麦_shuffle.txt','w',encoding='UTF-8') as s_f:
-            for line in lines:
-                s_f.write(line)
-
+    path=r'D:\博士期间相关资料\理论知识相关\知识图谱\知识图谱源码\ChineseNERAnno\data\水稻玉米小麦大豆大麦_shuffle_4.txt.ann.train'
+    save_path=r'D:\博士期间相关资料\理论知识相关\知识图谱\知识图谱源码\ChineseNERAnno\data\train_data.train'
+    _parse_anno(path,save_path)
